4d628a6cb3f41f5983f9208394b8f81d47a61958
ken594 <weihui99525@gmail.com>
2023-06-25 14:30:07 -0700

    Initial setup folder structure

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT;
+
+app.use(cors());
+require('./routes/product.routes')(app);
+
+app.listen(port, () => console.log(`Listening on port: ${port}`) );
