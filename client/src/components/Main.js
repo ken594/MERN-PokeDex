e2852c4ea114680cfaaab6d7d21e7764fe2c6995
Joseph Song <song.josephmin@gmail.com>
2023-06-27 22:10:43 -0700

    Beginning stages of landing page. #1

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,15 +1,42 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import { motion } from 'framer-motion'
+import styles from "../styles/global.module.css"
+import PokemonCard from './PokemonCard';
+
+
 export default () => {
     const [message, setMessage] = useState("Loading...")
-    useEffect(() => {
-        axios.get("http://localhost:8000/api")
-            .then(res => setMessage(res.data.message))
-    }, []);
+    // useEffect(() => {
+    //     axios.get("http://localhost:8000/api")
+    //         .then(res => setMessage(res.data.message))
+    // }, []);
     return (
-        <div>
-            <h2>Message from the backend: { message }</h2>
-            <h2>This is the first time I've tried this change</h2>
-        </div>
+        <>
+            <div className={ styles.pokemon_logo }>
+                <motion.img 
+                src='https://i.pinimg.com/originals/fd/18/c6/fd18c6d26d4d9d26a0bd9d1a2fb2bd04.png'
+                initial={{
+                    opacity: 0,
+                    y: -500
+                }}
+                animate={{
+                    opacity: 1,
+                    y: 0
+                }}
+                transition={{
+                    duration: 8,
+                    type: "spring",
+                    stiffness: 30,
+                    damping: 7
+                }}
+                className={ styles.pokemon_logo_img }
+                alt='pokemon_logo_img'>
+                </motion.img>
+            </div>
+
+            <div className={ styles.pokemon_container }>
+                <PokemonCard />
+            </div>
+        </>
     )
 }
---
+++ client/src/components/PokemonCard.jsx
@@ -0,0 +1,8 @@
+
+export default function PokemonCard(props) {
+    return (
+        <div className="border mt-5">
+            <h1>Bulbasaur</h1>
+        </div>
+    )
+}
