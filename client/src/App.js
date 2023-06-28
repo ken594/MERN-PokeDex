8ea5f25cacefe98ed4a8198ddccc0fe200b11f26
ken594 <weihui99525@gmail.com>
2023-06-28 10:07:49 -0700

    Chris fix

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
+      <h1 className="text-3xl font-bold underline">Hello World</h1>
       <Routes>
         <Route path="/" element={ <Main /> } />
         <Route path="/:pokemon" element={ <Main /> } />
